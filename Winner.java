a9388ad14471abd183ddc066f3f4f5b98d294d04
dkaldowski <37829237+dkaldowski@users.noreply.github.com>
2018-03-27 17:44:09 +0200

    Add files via upload

---
+++ Winner.java
@@ -0,0 +1,15 @@
+package com.naukaJavy.Bilbo;
+
+import java.util.List;
+
+public class Winner {
+    private List<Treasure> treasures;
+
+    public List<Treasure> getTreasures() {
+        return treasures;
+    }
+
+    public void setTreasures(List<Treasure> treasures) {
+        this.treasures = treasures;
+    }
+}
